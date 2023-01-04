e1fcada8756fa59cb069104ab0f6db92d590e56b
Francis Guido <fjguido97@gmail.com>
2023-01-04 13:05:20 -0500

    added Header component for items

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -107,10 +107,10 @@ const Sidebar = () => {
                   fontWeight="bold"
                   sx={{ m: "10px 0 0 0" }}
                 >
-                  Francis G
+                  Fra Gui
                 </Typography>
                 <Typography variant="h5" color={colors.greenAccent[500]}>
-                  VP Fancy Admin
+                  Administrator
                 </Typography>
               </Box>
             </Box>
