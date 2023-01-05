4fe410c733f6d4da743d044b4f6522f8cbf71e0e
Francis Guido <fjguido97@gmail.com>
2023-01-05 14:36:51 -0500

    changed routes

--- src/App.js
+++ src/App.js
@@ -28,17 +28,17 @@ function App() {
           <main className="content">
             <Topbar />
             <Routes>
-              <Route path="/home" element={<Dashboard />} />
-              <Route path="/team" element={<Team />} />
-              <Route path="/contacts" element={<Contacts />} />
-              <Route path="/invoices" element={<Invoices />} />
-              <Route path="/form" element={<Form />} />
-              <Route path="/bar" element={<Bar />} />
-              <Route path="/pie" element={<Pie />} />
-              <Route path="/line" element={<Line />} />
-              <Route path="/faq" element={<FAQ />} />
-              <Route path="/calendar" element={<Calendar />} />
-              <Route path="/geography" element={<Geography />} />
+              <Route path="dashboard/home" element={<Dashboard />} />
+              <Route path="dashboard/team" element={<Team />} />
+              <Route path="dashboard/contacts" element={<Contacts />} />
+              <Route path="dashboard/invoices" element={<Invoices />} />
+              <Route path="dashboard/form" element={<Form />} />
+              <Route path="dashboard/bar" element={<Bar />} />
+              <Route path="dashboard/pie" element={<Pie />} />
+              <Route path="dashboard/line" element={<Line />} />
+              <Route path="dashboard/faq" element={<FAQ />} />
+              <Route path="dashboard/calendar" element={<Calendar />} />
+              <Route path="dashboard/geography" element={<Geography />} />
             </Routes>
           </main>
         </div>
